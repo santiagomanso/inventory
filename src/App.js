3b0b324958fd3336c9043fcdd339d6ca10c5908b
Santiago <santiagomanso@hotmail.com>
2021-12-22 11:23:23 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import { Navbar } from './components/navbar/Navbar';
+import { Layout } from './components/layout/Layout';
+
+function App() {
+  return (
+    <Layout />
+  );
+}
+
+export default App;
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Navbar = () => {
+    return (
+        <nav className='bg-white p-5 sm:p-14 flex justify-between items-center h-16 text-black relative shadow-sm'>
+            <button className='bg-pink-600 py-1 sm:py-4 px-2 sm:px-7 text-white rounded-lg font-bold'>Home</button>
+            <h1>de_ber_moab</h1>
+            <span className='bg-neutral-500 py-1 sm:py-4  px-2 sm:px-7  text-white rounded-lg font-bold'>Logout</span>
+        </nav>
+    )
+}
