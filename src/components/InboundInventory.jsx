6a00ec1ba260322dae09f33c77c2f267e63368dc
Santiago <santiagomanso@hotmail.com>
2021-12-26 18:16:59 +0100

    UI - new colors

--- src/components/InboundInventory.jsx
+++ src/components/InboundInventory.jsx
@@ -13,7 +13,7 @@ export const InboundInventory = () => {
 
                 <div className="flex flex-col sm:flex-row mt-10 justify-stretch  ">
                     <input type="button" value="Scan mode: OFF" className="
-                    bg-pink-300 text-white cursor-pointer
+                    bg-slate-400 text-white cursor-pointer
                       text-left rounded-lg
                       p-4
                       sm:max-w-40
@@ -27,17 +27,22 @@ export const InboundInventory = () => {
                     rounded-xl
                     "/>
                     <input type="button" value="Check in" className="
-                    bg-zinc-300 rounded-lg hidden sm:block
-                      sm:w-40
-                      mr-4
+                    bg-slate-400 rounded-lg hidden sm:block
+                    sm:w-40 mr-4
+                    text-white font-bold text-lg
                     " /> 
+                    
                 </div>
 
                 <div className="bg-white p-20 sm:p-28 h-1/3 rounded-lg mt-10 text-center">
                     <h2 className='font-semibold'>Search or scan a product to start</h2>
                 </div>
                 
-                <input type="button" value="Check in" className="bg-gray-300 p-4 rounded-lg cursor-pointer w-5/6 bottom-8 fixed sm:hidden" />    
+                <input type="button" value="Check in" className="
+                bg-red-500 p-4 rounded-lg cursor-pointer
+                w-5/6 bottom-8 fixed sm:hidden
+                text-white font-bold text-lg
+                "/>    
         </div>
            
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,12 +6,12 @@ export const Navbar = () => {
     const navigate = useNavigate();
     return (
         <nav className='bg-white p-5 sm:p-14 flex justify-between items-center h-16 text-black relative shadow-sm'>
-            <button className='bg-pink-600 py-1 sm:py-4 px-2 sm:px-7 text-white rounded-lg font-bold'
+            <button className='bg-slate-600 py-1 sm:py-4 px-2 sm:px-7 text-white rounded-lg font-bold'
                 onClick={()=>{navigate('../')}}>
                 Home
             </button>
             <h1 className='text-xl sm:text-4xl font-bold'>user.username</h1>
-            <span className='bg-neutral-400 py-1 sm:py-4  px-2 sm:px-7  text-white rounded-lg font-bold'>Logout</span>
+            <span className='bg-red-500 py-1 sm:py-4  px-2 sm:px-7  text-white rounded-lg font-bold'>Logout</span>
         </nav>
     )
 }
