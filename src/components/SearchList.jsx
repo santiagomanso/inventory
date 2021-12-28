8d65441e91fb918611b99150df4b3c24242845dd
Santiago <santiagomanso@hotmail.com>
2021-12-28 23:48:29 +0100

    forgot css from tailwind sandbox

--- src/components/SearchList.jsx
+++ src/components/SearchList.jsx
@@ -62,6 +62,42 @@ export const SearchList = () => {
                           </div>
                         </div>
                       </div>
+                      <div className="flex flex-row justify-start ">
+                        <div className=" py-2 px-2">
+                          <img src="https://res.cloudinary.com/goflink/image/upload/w_600,h_800/de/products/1_11012025_202110271251.png" alt="Girl in a jacket" className="h-28 w-26" />
+                        </div>
+                        <div className="flex flex-col justify-center items-center "> 
+                          <p className="font-bold">Coca Cola Zero 0,33l</p>
+                          <div className="flex flex-row self-start"> 
+                            <p className="mr-3"> 11012025 </p>
+                            <p className="font-bold"> #007F </p>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="flex flex-row justify-start ">
+                        <div className=" py-2 px-2">
+                          <img src="https://res.cloudinary.com/goflink/image/upload/w_600,h_800/de/products/1_11012025_202110271251.png" alt="Girl in a jacket" className="h-28 w-26" />
+                        </div>
+                        <div className="flex flex-col justify-center items-center "> 
+                          <p className="font-bold">Coca Cola Zero 0,33l</p>
+                          <div className="flex flex-row self-start"> 
+                            <p className="mr-3"> 11012025 </p>
+                            <p className="font-bold"> #007F </p>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="flex flex-row justify-start ">
+                        <div className=" py-2 px-2">
+                          <img src="https://res.cloudinary.com/goflink/image/upload/w_600,h_800/de/products/1_11012025_202110271251.png" alt="Girl in a jacket" className="h-28 w-26" />
+                        </div>
+                        <div className="flex flex-col justify-center items-center "> 
+                          <p className="font-bold">Coca Cola Zero 0,33l</p>
+                          <div className="flex flex-row self-start"> 
+                            <p className="mr-3"> 11012025 </p>
+                            <p className="font-bold"> #007F </p>
+                          </div>
+                        </div>
+                      </div>
 
                     </div>
     )
